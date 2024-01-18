188942e4a05ece9342acf28d7ebe80e4b4c05216
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-01-18 21:45:47 +0530

    style: ui part completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       fontFamily: {
         Sen: ["Sen"],
+      },
+      colors: {
+        primary: "#352F44",
+        secondary: "#B9B4C7",
+        textPrimary: "#FAF0E6",
+        textSecondary: "#e1d8cf",
+        tableSecondary: "#5d5869",
       },
     },
   },
